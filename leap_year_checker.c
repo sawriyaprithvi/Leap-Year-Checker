396d7a569ee1d0dd918ee2da32581351b6e69e53
sawriyaprithvi <136826086+sawriyaprithvi@users.noreply.github.com>
2023-06-19 12:38:43 +0530

    leap_year_checker.c

--- leap_year_checker.c
+++ leap_year_checker.c
@@ -2,13 +2,13 @@
 
 int main(){
     int year;
-    printf("Enter the year");
+    printf("Enter the year\n");
     scanf("%d", &year);
    if(year/4==0){
-    printf("The year is leap year");
+    printf("The year is leap year\n");
    }
    else{
-    printf("The year is not a leap year");
+    printf("The year is not a leap year\n");
    }
     
     return 0;
